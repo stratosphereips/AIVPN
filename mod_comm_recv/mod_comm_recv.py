cc5e5f76c9fedf78bfc12b567e83cd9e00e6ef89
verovaleros <vero.valeros@gmail.com>
2020-12-13 21:07:46 +0000

    Updated to use common db library

--- mod_comm_recv/mod_comm_recv.py
+++ mod_comm_recv/mod_comm_recv.py
@@ -6,27 +6,7 @@
 import sys
 import redis
 import logging
-
-def redis_connect_to_db(REDIS_SERVER):
-    try:
-        publisher = redis.Redis(REDIS_SERVER, port=6379, db=0)
-        return publisher
-    except Exception as err:
-        return err
-
-def redis_create_subscriber(publisher):
-    try:
-        subscriber = publisher.pubsub()
-        return subscriber
-    except Exception as err:
-        return err
-
-def redis_subscribe_to_channel(subscriber,CHANNEL):
-    try:
-        subscriber.subscribe(CHANNEL)
-        return True
-    except Exception as err:
-        return err
+from common.database import *
 
 if __name__ == '__main__': 
     REDIS_SERVER = 'aivpn_mod_redis'
