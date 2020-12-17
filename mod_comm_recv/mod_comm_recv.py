fe7f71d248af292b383646a4abe0f06f346c8f20
verovaleros <vero.valeros@gmail.com>
2020-12-17 15:59:46 +0100

    Act upon report_status messages from manager
    Added parsing and response to report_status messages from manager.

--- mod_comm_recv/mod_comm_recv.py
+++ mod_comm_recv/mod_comm_recv.py
@@ -48,6 +48,8 @@ if __name__ == '__main__':
             if item['type'] == 'message':
                 logging.info(item['channel'])
                 logging.info(item['data'])
+                if item['data'] == 'report_status':
+                    db_publisher.publish('services_status', 'MOD_COMM_RECV:online')
 
         db_publisher.publish('services_status', 'MOD_COMM_RECV:offline')
         logging.info("Terminating.")
