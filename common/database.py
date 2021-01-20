4023bb3bcd946feead2f07006f097a6dd72a73b8
verovaleros <vero.valeros@gmail.com>
2021-01-20 14:48:53 +0100

    Updated author header

--- common/database.py
+++ common/database.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# This file is part of the Civilsphere AI VPN
+# See the file 'LICENSE' for copying permission.
+# Author: Veronica Valeros, vero.valeros@gmail.com, veronica.valeros@aic.fel.cvut.cz
+
 import redis
 
 def redis_connect_to_db(REDIS_SERVER):
