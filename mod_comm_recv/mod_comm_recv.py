1ee8b780c1391f8d7c83a5c0be7bdb8cb06d7d0a
verovaleros <vero.valeros@gmail.com>
2021-01-20 15:35:25 +0100

    Ported function to check for new emails

--- mod_comm_recv/mod_comm_recv.py
+++ mod_comm_recv/mod_comm_recv.py
@@ -7,18 +7,132 @@ import sys
 import redis
 import logging
 import configparser
+import re
+import imaplib
+from email.parser import BytesFeedParser
 from common.database import *
 
-def send_request_to_redis():
+def send_request_to_redis(email_uid, email_date, email_from):
     """
     This function writes a new AI-VPN request to Redis.
     This is the first step to get a new account provisioned.
     """
     try:
-        print("Sending a request to Redis")
+        print("Sending a request to Redis: ({}) {} on {}".format(email_uid,email_from,email_date))
         return True
     except Exception as e:
         print(e)
+
+def get_new_requests(db_publisher):
+    email_requests = []
+
+    try:
+        # Connect to email
+        mail = imaplib.IMAP4_SSL(SERVER)
+        mail.login(USERNAME,PASSWORD)
+
+        # Connect to Inbox
+        mail.select("Inbox", readonly=False)
+
+        # Search and return UIDS of all UNSEEN/UNREAD emails in Inbox
+        result, data = mail.uid('search', None, "UNSEEN")
+
+        # We receive a list of unread email UID
+        id_list = data[0].split() # data is a list.
+
+        # Process the new unread emails. If zero, nothing returns
+        while len(id_list) > 0:
+            # In case another process checked emails simultaneously, we refresh the list of ids
+            result, data = mail.uid('search', None, "UNSEEN")
+            id_list = data[0].split() # data is a list.
+
+            # Get the first email ID to process
+            email_uid = id_list.pop(0)
+
+            # Fetch the email headers and body (RFC822) for the given email UID
+            result, data = mail.uid('fetch', email_uid, '(RFC822)')
+
+            # Parse email to extract header and body
+            email_parser = BytesFeedParser()
+            email_parser.feed(data[0][1])
+            msg = email_parser.close()
+
+            # Do not process answers to the emails we send
+            if msg['In-Reply-To'] is not None:
+                continue
+
+            # Parse email receiver
+            email_to = re.search(r'[\w\.-]+@[\w\.-]+', msg['to']).group(0)
+
+            # Do not process messages where we are not the receivers
+            if not email_to == USERNAME:
+                continue
+
+            # Parse subject and find matches for keyword VPN
+            try:
+                email_subject = re.search(r'[VPN]+', msg['subject']).group(0)
+            except:
+                email_subject = ""
+
+            # Parse email body and find matches for keyword VPN
+            try:
+                # Extract email body in rich email
+                email_body = msg.get_payload().pop().get_payload()
+            except:
+                # Extract email body in plain email
+                email_body = msg.get_payload()
+
+            try:
+                email_body = re.search(r'[VPN]+', email_body).group(0)
+            except:
+                email_body = ""
+
+
+            # We only parse messages that contain VPN in subject or body
+            # These prints will be removed after we test everythig is good
+            if (email_subject == 'VPN' or email_body == 'VPN') and email_to == USERNAME:
+                # Parse email date
+                email_date = msg['date']
+
+                # Parse email sender
+                email_from = re.search(r'[\w\.-]+@[\w\.-]+', msg['from']).group(0)
+
+                # Write pending account to provision in REDIS
+                send_request_to_redis(int(email_uid),email_date,email_from)
+
+                # Notify manager of new request
+                db_publisher.publish('services_status', 'MOD_COMM_RECV:NEW_REQUEST')
+                logging.info('MOD_COMM_RECV::New Request::{}::{}::{}'.format(int(email_uid),email_from,email_date))
+
+                email_requests.append([int(email_uid),email_date,email_from])
+                print('{:8}: {}'.format("email id",int(email_uid)))
+                print('{:8}: {}'.format("date",email_date))
+                print('{:8}: {}'.format("to",email_to))
+                print('{:8}: {}'.format("from",email_from))
+                print('{:8}: {}'.format("reply_to",msg['In-Reply-To']))
+                print('{:8}: {}'.format("subject",email_subject))
+                print('{:8}: {}'.format("body",email_body))
+                print()
+            else:
+                print("This email does not match the keywords")
+                print('{:8}: {}'.format("Email ID",email_uid))
+                print('{:8}: {}'.format("Date",email_date))
+                print('{:8}: {}'.format("To",email_to))
+                print('{:8}: {}'.format("From",email_from))
+                print('{:8}: {}'.format("reply_to",msg['In-Reply-To']))
+                print('{:8}: {}'.format("Subject",email_subject))
+                print('{:8}: {}'.format("Body",email_body))
+                print()
+
+        # Close connection to server
+        mail.expunge()
+        mail.close()
+        mail.logout()
+        return True
+    except Exception as e:
+        print(e)
+        # Notify manager of error
+        return False
 
 if __name__ == '__main__': 
     # Initialize logging
@@ -65,16 +179,12 @@ if __name__ == '__main__':
                 logging.info(item['channel'])
                 logging.info(item['data'])
                 if item['data'] == b'report_status':
-                    # Check for new emails
-                    # Load credentials
-                    # check_new_requests()
-                    #    - Parse the email to find valid requests (only VPN requests, etc)
-                    # If there are new requests:
-                    #    - Write request to REDIS with status PENDING (send_request_to_redis)
-                    #    - Answer manager with message indicating there are new requests to process.
-                    # If there are no new requests, report OK
-                    db_publisher.publish('services_status', 'MOD_COMM_RECV:online')
-                    logging.info('MOD_COMM_RECV:online')
+                    if get_new_requests(db_publisher):
+                        db_publisher.publish('services_status', 'MOD_COMM_RECV:online')
+                        logging.info('MOD_COMM_RECV:online')
+                    else:
+                        db_publisher.publish('services_status', 'MOD_COMM_RECV:error_checking_requests')
+                        logging.info('MOD_COMM_RECV:error_checking_requests')
 
         db_publisher.publish('services_status', 'MOD_COMM_RECV:offline')
         logging.info("Terminating.")
