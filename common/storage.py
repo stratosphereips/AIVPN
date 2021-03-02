97fef7d209e3eb614a5047a8c6e85c135bc0321a
verovaleros <vero.valeros@gmail.com>
2021-03-02 14:25:41 +0100

    Added common storage library to handle files

---
+++ common/storage.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# This file is part of the Civilsphere AI VPN
+# See the file 'LICENSE' for copying permission.
+# Author: Veronica Valeros, vero.valeros@gmail.com, veronica.valeros@aic.fel.cvut.cz
