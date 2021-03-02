d617d9be72d83e6447b7dfbb71a582deb19eb52e
verovaleros <vero.valeros@gmail.com>
2021-03-02 15:03:50 +0100

    Added create_working_directory() function to storage library

--- common/storage.py
+++ common/storage.py
@@ -2,3 +2,25 @@
 # This file is part of the Civilsphere AI VPN
 # See the file 'LICENSE' for copying permission.
 # Author: Veronica Valeros, vero.valeros@gmail.com, veronica.valeros@aic.fel.cvut.cz
+
+import os
+import configparser
+
+# Read the file path to be used from config file
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+PATH = config['STORAGE']['PATH']
+
+def create_working_directory(profile_name,PATH):
+    """
+    Create a working directory to store files related with a profile_name.
+    """
+    profile_directory = PATH+"/"+profile
+
+    try:
+        if not os.path.exists(profile_directory):
+            os.makedirs(profile_directory)
+        return True
+    except OSError:
+        return False
+
