a09aa247237ef8c3387483cd52392f826fe0c8bb
verovaleros <vero.valeros@gmail.com>
2021-03-03 13:22:50 +0100

    Adding examples of how to create the tun device by hand

--- mod_openvpn/mod_openvpn.sh
+++ mod_openvpn/mod_openvpn.sh
@@ -8,10 +8,15 @@
 vpn.civilsphere.org
 HERE
 
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
+/usr/sbin/openvpn --config /etc/openvpn/openvpn.conf --client-config-dir /etc/openvpn/ccd --crl-verify /etc/openvpn/pki/crl.pem
 # Example of how to generate a client cert
-/usr/local/bin/easyrsa build-client-full CLIENTNAME nopass
+#/usr/local/bin/easyrsa build-client-full CLIENTNAME nopass
 # And to retrieve it
-/usr/local/bin/ovpn_getclient CLIENTNAME > /certs/CLIENTNAME1.ovpn
+#/usr/local/bin/ovpn_getclient CLIENTNAME > /certs/CLIENTNAME1.ovpn
 
 # Run now the python that manages the openvpn channel
-python3 /code/mod_openvpn.py
+#python3 /code/mod_openvpn.py
