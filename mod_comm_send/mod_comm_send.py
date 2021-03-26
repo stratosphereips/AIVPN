64d24ad5dd2430b33fbfc3dcac431b3134c58ee9
verovaleros <vero.valeros@gmail.com>
2021-03-26 15:12:28 +0100

    Added logic for parsing multiple sending messages

--- mod_comm_send/mod_comm_send.py
+++ mod_comm_send/mod_comm_send.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     # Read configuration
     REDIS_SERVER,CHANNEL,LOG_FILE,IMAP_SERVER,IMAP_USERNAME,IMAP_PASSWORD = read_configuration()
 
-    logging.basicConfig(filename=LOG_FILE, encoding='utf-8', level=logging.DEBUG,format='%(asctime)s, MOD_CONN_SEND, %(message)s')
+    logging.basicConfig(filename=LOG_FILE, encoding='utf-8', level=logging.DEBUG,format='%(asctime)s, MOD_COMM_SEND, %(message)s')
 
     # Connecting to the Redis database
     try:
@@ -49,17 +49,26 @@ if __name__ == '__main__':
         logging.error("Channel subscription failed")
         sys.exit(-1)
 
-    try: 
+    try:
         logging.info("Connection and channel subscription to redis successful.")
 
         # Checking for messages
         for item in db_subscriber.listen():
             if item['type'] == 'message':
-                logging.info(item['channel'])
-                logging.info(item['data'])
+                logging.info("New message received in channel {}: {}".format(item['channel'],item['data']))
+                try:
+                    msg_account_name=item['data'].split(':')[1]
+                except:
+                    msg_account_name=""
                 if item['data'] == 'report_status':
                     redis_client.publish('services_status', 'MOD_COMM_SEND:online')
                     logging.info('MOD_COMM_SEND:online')
+                elif 'send_openvpn_profile_email' in item['data'] and not msg_account_name=="":
+                    logging.info('Sending OpenVPN profile to {}'.format(msg_account_name))
+                elif 'send_expire_profile_email' in item['data'] and not msg_account_name=="":
+                    logging.info('Sending expiration of profile to {}'.format(msg_account_name))
+                elif 'send_report_profile_email' in item['data'] and not msg_account_name=="":
+                    logging.info('Sending report on profile to {}'.format(msg_account_name))
 
         redis_client.publish('services_status', 'MOD_COMM_SEND:offline')
         logging.info("Terminating")
