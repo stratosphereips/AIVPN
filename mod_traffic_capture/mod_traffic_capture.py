cedd3b863bcb86561e055b6c3f725ec7f83349c3
verovaleros <vero.valeros@gmail.com>
2021-03-29 17:41:22 +0200

    Code refactoring of mod_traffic_capture

--- mod_traffic_capture/mod_traffic_capture.py
+++ mod_traffic_capture/mod_traffic_capture.py
@@ -9,7 +9,8 @@ import logging
 import configparser
 from common.database import *
 
-if __name__ == '__main__':
+def read_configuration():
+    # Read configuration
     config = configparser.ConfigParser()
     config.read('config/config.ini')
 
@@ -17,35 +18,32 @@ if __name__ == '__main__':
     CHANNEL = config['REDIS']['REDIS_TRAFFIC_CAPTURE_CHECK']
     LOG_FILE = config['LOGS']['LOG_TRAFFIC_CAPTURE']
 
-    try:
-        logging.basicConfig(filename=LOG_FILE, encoding='utf-8', level=logging.DEBUG,format='%(asctime)s, MOD_TRAFFIC_CAPTURE, %(message)s')
-    except Exception as err:
-        logging.info("Exception in __main__: {}".format(err))
-        sys.exit(-1)
+    return REDIS_SERVER,CHANNEL,LOG_FILE
 
-    try:
-        from common.swarm_modules import *
-        REDIS_SERVER=aivpn_mod_redis
-    except Exception as err:
-        logging.info("Exception in __main__: {}".format(err))
-        logging.info("Cannot retrieve redis IP from manager")
+if __name__ == '__main__':
+    REDIS_SERVER,CHANNEL,LOG_FILE = read_configuration()
 
-    # Connecting to the Redis database
+    logging.basicConfig(filename=LOG_FILE, encoding='utf-8', level=logging.DEBUG,format='%(asctime)s, MOD_TRAFFIC_CAPTURE, %(message)s')
+
+    # Connecting to the Redis database. First to the address specified in the
+    # configuration file. Second from the file created by the mod_manager.
     try:
-        db_publisher = redis_connect_to_db(REDIS_SERVER)
+        redis_client = redis_connect_to_db(REDIS_SERVER)
         logging.info("Connection to redis successful.")
     except Exception as err:
-        logging.info("Exception in __main__: {}".format(err))
-        logging.error("Unable to connect to the Redis database (",REDIS_SERVER,")")
-        sys.exit(-1)
+        try:
+            from common.swarm_modules import *
+            REDIS_SERVER=aivpn_mod_redis
+        except Exception as err:
+            logging.info(f'Exception in __main__: {err}. Cannot retrieve Redis IP: {REDIS_SERVER}')
+            sys.exit(-1)
 
     # Creating a Redis subscriber
     try:
-        db_subscriber = redis_create_subscriber(db_publisher)
+        db_subscriber = redis_create_subscriber(redis_client)
         logging.info("Redis subscriber created.")
     except Exception as err:
-        logging.info("Exception in __main__: {}".format(err))
-        logging.error("Unable to create a Redis subscriber")
+        logging.info(f'Exception in __main__: {err}. Unable to create Redis subscriber.')
         sys.exit(-1)
 
     # Subscribing to Redis channel
@@ -53,28 +51,26 @@ if __name__ == '__main__':
         redis_subscribe_to_channel(db_subscriber,CHANNEL)
         logging.info("Subscribed to channel: {}".format(CHANNEL))
     except Exception as err:
-        logging.info("Exception in __main__: {}".format(err))
-        logging.error("Channel subscription failed")
+        logging.info(f'Exception in __main__: {err}. Channel subscription failed.')
         sys.exit(-1)
 
     try:
         # Checking for messages
         logging.info("Listening for messages.")
         for item in db_subscriber.listen():
-            logging.info(item)
             if item['type'] == 'message':
                 logging.info("New message received in channel {}: {}".format(item['channel'],item['data']))
                 if item['data'] == 'report_status':
-                    db_publisher.publish('services_status', 'MOD_TRAFFIC_CAPTURE:online')
+                    redis_client.publish('services_status', 'MOD_TRAFFIC_CAPTURE:online')
                     logging.info('MOD_TRAFFIC_CAPTURE:online')
 
-        db_publisher.publish('services_status', 'MOD_TRAFFIC_CAPTURE:offline')
+        redis_client.publish('services_status', 'MOD_TRAFFIC_CAPTURE:offline')
         logging.info("Terminating")
-        db_publisher.close()
         db_subscriber.close()
+        redis_client.close()
         sys.exit(0)
     except Exception as err:
         logging.info("Exception in __main__: {}".format(err))
-        db_publisher.close()
         db_subscriber.close()
+        redis_client.close()
         sys.exit(-1)
