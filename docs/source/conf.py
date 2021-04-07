f54be2961c8d02613a196a4d529a8f0fb7a0c927
verovaleros <vero.valeros@gmail.com>
2021-04-07 14:50:38 +0200

    docs: pdated sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,15 +39,14 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build']
 
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
