b3fc6741f391598d3dfc8dfb9c443af4f8f0cc05
verovaleros <vero.valeros@gmail.com>
2021-04-14 19:04:58 +0200

    Added handling of empty capture notification

--- mod_comm_send/mod_comm_send.py
+++ mod_comm_send/mod_comm_send.py
@@ -128,6 +128,28 @@ def send_expired_profile_msg_via_email(msg_account_name,msg_address,SMTP_HOST,SM
     except Exception as err:
         return err
 
+def send_empty_capture_msg_via_email(msg_account_name,msg_address,SMTP_HOST,SMTP_USER,SMTP_PASSWORD):
+    """ Function to send the message that profile expired to the user via email. """
+    try:
+        # Craft the email by hand
+        body = "Thank you for using our Emergency VPN service. Unfortunately, there was no traffic captured in this Emergency VPN session. Please try again."
+        headers = f"From: {SMTP_USER}\r\n"
+        headers += f"To: {msg_address}\r\n"
+        headers += f"Subject: [CivilSphere Emergency VPN] Emergency VPN Report: {msg_account_name}\r\n"
+        email_message = headers + "\r\n" + body  # Blank line needed between headers and body
+
+        # Connect, authenticate, and send mail
+        smtp_server = SMTP_SSL(SMTP_HOST, port=SMTP_SSL_PORT)
+        smtp_server.set_debuglevel(1)  # Show SMTP server interactions
+        smtp_server.login(SMTP_USER, SMTP_PASSWORD)
+        smtp_server.sendmail(SMTP_USER, msg_address, email_message)
+
+        # Disconnect
+        smtp_server.quit()
+        return True
+    except Exception as err:
+        return err
+
 def send_profile_report_via_email(msg_account_name,msg_address,SMTP_HOST,SMTP_USER,SMTP_PASSWORD,PATH):
     """ Function to send the profile report to the user via email. """
     try:
@@ -135,7 +157,7 @@ def send_profile_report_via_email(msg_account_name,msg_address,SMTP_HOST,SMTP_US
         email_message = MIMEMultipart()
         email_message.add_header('To', msg_address)
         email_message.add_header('From', SMTP_USER)
-        email_message.add_header('Subject', f"[Civilsphere Emergency VPN] New profile: {msg_account_name}\r\n")
+        email_message.add_header('Subject', f"[Civilsphere Emergency VPN] Emergency VPN Report: {msg_account_name}\r\n")
 
         # Create text and HTML bodies for email
         text_part = MIMEText('Please find attached your new Emergency VPN profile.', 'plain')
@@ -223,6 +245,11 @@ if __name__ == '__main__':
                             redis_client.publish('services_status', 'MOD_COMM_SEND:failed to send profile expiration message')
                             logging.info('failed to send profile expiration message')
                         continue
+                    if 'send_empty_capture_email' in item['data']:
+                        logging.info(f'Sending report that capture was empty to profile {msg_account_name} ({msg_address})')
+                        if send_empty_capture_msg_via_email(msg_account_name,msg_address,IMAP_SERVER,IMAP_USERNAME,IMAP_PASSWORD):
+                            redis_client.publish('services_status', 'MOD_COMM_SEND:profile report sent successfully')
+                            logging.info('profile report sent successfully')
                     if 'send_report_profile_email' in item['data']:
                         logging.info('Sending report on profile to {} ({})'.format(msg_account_name,msg_address))
                         if send_profile_report_via_email(msg_account_name,msg_address,IMAP_SERVER,IMAP_USERNAME,IMAP_PASSWORD,PATH):
