0972d5743f0b4d6bd30d8af1fa8d7befc16c0324
verovaleros <vero.valeros@gmail.com>
2021-04-19 11:52:50 +0200

    Store output in JSON format (capinfos, tshark http request)

--- mod_report/pcapsummarizer.sh
+++ mod_report/pcapsummarizer.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 VERSION=0.2
 PCAP=$1
+FILENAME="${PCAP%.*}"
 IFS=$'\n'
 
 # Verifying that parameters were given
@@ -52,3 +53,13 @@ echo
 
 echo
 tcpdump -nn -s0 -r $PCAP port 80 -A 2>/dev/null |grep "\":{\""| grep -i "wifi\|chrome\|access\|en\|cz\|es\|lang\|com\|loc\|lat\|lon\|imei\|mn\|android\|ios\|build\|time\|format\|[0-9][0-9]\."|sort|uniq -c |sort -n -k 1 -r | sed 's/^/     /'
+
+# This tool will generate the output in JSON format. The files will later be
+# processed and assembled together by the report module in order to generate
+# the markdown and the PDF.
+
+# Capture information in JSON format
+capinfos -TmQ $PCAP | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > $FILENAME".capinfos"
+
+# HTTP Request information in JSON format
+tshark -r $PCAP -Y http.request -T json -e http.host -e http.request.method -e http.request.uri -e http.user_agent -e http.request.version 2>/dev/null > $FILENAME".http"
