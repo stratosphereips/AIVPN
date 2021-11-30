895ec9729a3d7060995c5630b269ed6929d0e759
verovaleros <vero.valeros@gmail.com>
2021-11-30 08:37:02 +0100

    Adding processing of zeek files

--- mod_report/pcapsummarizer.sh
+++ mod_report/pcapsummarizer.sh
@@ -7,6 +7,8 @@ set -o pipefail
 VERSION=0.2
 PCAP=$1
 FILENAME="${PCAP%.*}"
+DIRNAME=$(dirname $FILENAME)
+BASENAME=$(basename $FILENAME)
 IFS=$'\n'
 
 # Verifying that parameters were given
@@ -31,5 +33,27 @@ tshark -r $PCAP -T json -Y dns -e dns.qry.name 2>/dev/null | tr '\n' ' ' |tr -s 
 # Top 10 Uploads JSON format
 tshark -qzconv,ipv4 -r $PCAP 2>/dev/null |grep -v "|\|IPv4\|Filter\|=" |sort -n -k 7 -r | head -n 10 | awk 'BEGIN{print "Source-Destination,Total Download,Total Upload,Total Transferred,Duration"}; {print $1" "$2" "$3","$5","$7","$9","$11}; END{}' | python -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > $FILENAME".uploads"
 
+# Get Slips ZEEK stats
+ZEEK=$(echo "$DIRNAME/slips_$BASENAME.pcap/zeek_files")
+CONN=0 ; DNS=0; HTTP=0; SSL=0
+if [ -d "$ZEEK" ];
+then
+        CONN=$(cat $ZEEK/conn.log | grep -v "#" | wc -l)
+
+        if [ -f "$ZEEK/http.log" ]; then
+                HTTP=$(cat $ZEEK/http.log | grep -v "#" | wc -l)
+        fi
+
+        if [ -f "$ZEEK/dns.log" ]; then
+                DNS=$(cat $ZEEK/dns.log | grep -v "#" | wc -l)
+        fi
+
+        if [ -f "$ZEEK/ssl.log" ]; then
+                SSL=$(cat $ZEEK/ssl.log | grep -v "#" | wc -l)
+        fi
+
+        zeek_json=$(echo '{ "connections": '$CONN', "dns": '$DNS', "http": '$HTTP', "ssl": '$SSL' }')
+fi
+
 # Create unique JSON
-echo "{ \"capinfos\": $(cat $FILENAME.capinfos),\"top_dns\":$(cat $FILENAME.dns),\"top_uploads\":$(cat $FILENAME.uploads),\"insecure_http\":$(cat $FILENAME.http)}" > $FILENAME".json"
+echo "{ \"capinfos\": $(cat $FILENAME.capinfos),\"top_dns\":$(cat $FILENAME.dns),\"top_uploads\":$(cat $FILENAME.uploads),\"insecure_http\":$(cat $FILENAME.http), \"zeek\": $zeek_json}" > $FILENAME".json"
