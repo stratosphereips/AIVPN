a7af22798306030f8e087cc9d82bb38721c8487b
verovaleros <vero.valeros@gmail.com>
2022-10-24 19:52:18 +0200

    Rename sections to commands and actions

--- aivpn.py
+++ aivpn.py
@@ -20,22 +20,24 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "AI VPN Command Line Tool")
     parser.add_argument( "-v", "--verbose", help="increase output verbosity", action="store_true")
 
-    # Configure subparsers
+    # Configure commands
     subparser = parser.add_subparsers(dest='command')
     manage = subparser.add_parser('manage')
     provision = subparser.add_parser('provision')
     audit = subparser.add_parser('audit')
 
-    # manage arguments
+    # manage actions
     manage.add_argument('--info', help='retrieve information of a profile', type=str, required=True)
     manage.add_argument('--expire', help='expire a profile', type=str, required=True)
     manage.add_argument('--extend', help='extend the expiration of a profile (add default expiration on top of current date)', type=str, required=True)
     manage.add_argument('--whois', help='retrieve identity associated with a profile', type=str, required=True)
 
-    # provision arguments
+    # provision actions
     provision.add_argument('--new-openvpn', help='create a new openvpn profile for a given identity (email|telegram)', type=str, required=True)
     provision.add_argument('--new-wireguard', help='create a new wireguard profile for a given identity (email|telegram)', type=str, required=True)
     provision.add_argument('--new-novpn', help='create a new novpn profile for a given identity (email|telegram)', type=str, required=True)
+
+    # audit actions
 
     args = parser.parse_args()
 
