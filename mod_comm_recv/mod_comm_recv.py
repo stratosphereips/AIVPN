f210e43c3c8b8d02888a76ab59e2266c6310f0e5
alya <alyaggomaa@gmail.com>
2022-10-26 18:01:00 +0200

    log info when receiving n eduvpn reuqest email

--- mod_comm_recv/mod_comm_recv.py
+++ mod_comm_recv/mod_comm_recv.py
@@ -195,9 +195,11 @@ def get_email_requests(redis_client,IMAP_SERVER,IMAP_USERNAME,IMAP_PASSWORD):
                         try:
                             email_body = re.search(r'EDUVPN',email_body,re.IGNORECASE).group(0)
                             msg_request="eduvpn"
+                            logging.info('*********FOUND EDUVPN REQUEST IN MAIL*************')
                         except:
                             email_body = re.search(r'VPN',email_body,re.IGNORECASE).group(0)
                             msg_request="openvpn"
+                            logging.info('*********FOUND openvpn REQUEST IN MAIL*************')
 
 
 
