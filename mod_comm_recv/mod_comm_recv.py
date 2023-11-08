124be8b1243d7326a2f036b37855425963524735
verovaleros <vero.valeros@gmail.com>
2023-11-08 14:43:40 +0100

    Avoid lazy formatting in logging

--- mod_comm_recv/mod_comm_recv.py
+++ mod_comm_recv/mod_comm_recv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+# pylint: disable=C0301
 # This file is part of the Civilsphere AI VPN
 # See the file 'LICENSE' for copying permission.
-# Author: Veronica Valeros, vero.valeros@gmail.com, veronica.valeros@aic.fel.cvut.cz
+# Author: Veronica Valeros
+#         veronica.valeros@aic.fel.cvut.cz
 from functools import partial
 import sys
 import logging
@@ -9,6 +11,10 @@ import re
 import imaplib
 import threading
 from email.parser import BytesFeedParser
+from common.database import add_item_provisioning_queue
+from common.database import redis_connect_to_db
+from common.database import redis_create_subscriber
+from common.database import redis_subscribe_to_channel
 from common.database import *
 from telegram.ext import CommandHandler, Updater
 
@@ -19,11 +25,19 @@ def send_request_to_redis(msg_id, msg_addr, msg_type, msg_request, logging, redi
     This is the first step to get a new account provisioned.
     """
     try:
-        logging.debug(f'Sending {msg_request} request to Redis: ({str(msg_id)}) {msg_addr} on {msg_type}')
-        add_item_provisioning_queue(redis_client, msg_id, msg_type, msg_addr, msg_request)
+        logging.debug("Sending %s request to Redis: (%s) {%s} on {%s}",
+                      msg_request,
+                      str(msg_id),
+                      msg_addr,
+                      msg_type)
+        add_item_provisioning_queue(redis_client,
+                                    msg_id,
+                                    msg_type,
+                                    msg_addr,
+                                    msg_request)
         return True
     except Exception as err:
-        logging.info(f'Exception in send_request_to_redis: {err}')
+        logging.info("Exception in send_request_to_redis: %s", err)
         return False
 
 
@@ -36,14 +50,20 @@ def get_telegram_requests(redis_client, TELEGRAM_BOT_TOKEN, TELEGRAM_START_MSG, 
     # Telegram Handlers
     def telegram_cmd_start(update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text=TELEGRAM_START_MSG)
-        logging.info('New Telegram chat received')
+        logging.info("New Telegram chat received")
 
     def telegram_cmd(update, context, msg_request):
 
         context.bot.send_message(chat_id=update.effective_chat.id, text=TELEGRAM_WAIT_MSG)
-        logging.info(f'New Telegram {msg_request} request received from: {update.effective_chat.id}')
+        logging.info("New Telegram %s request received from: %s",
+                     msg_request,
+                     update.effective_chat.id)
         # Write pending account to provision in REDIS
-        send_request_to_redis(int(update.effective_chat.id), update.effective_chat.id, msg_type, msg_request, logging,
+        send_request_to_redis(int(update.effective_chat.id),
+                              update.effective_chat.id,
+                              msg_type,
+                              msg_request,
+                              logging,
                               redis_client)
         # Notify manager of new request
         redis_client.publish('services_status', 'MOD_COMM_RECV:NEW_REQUEST')
@@ -52,34 +72,37 @@ def get_telegram_requests(redis_client, TELEGRAM_BOT_TOKEN, TELEGRAM_START_MSG, 
         # Initializing
         updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
         dispatcher = updater.dispatcher
-        logging.info('Telegram bot initialized')
+        logging.info("Telegram bot initialized")
 
         # Creating handlers per action
         start_handler = CommandHandler('start', telegram_cmd_start)
         dispatcher.add_handler(start_handler)
 
-        openvpn_handler = CommandHandler('getopenvpn', partial(telegram_cmd, "openvpn"))
+        openvpn_handler = CommandHandler('getopenvpn',
+                                         partial(telegram_cmd, "openvpn"))
         dispatcher.add_handler(openvpn_handler)
 
-        wireguard_handler = CommandHandler('getwireguard', partial(telegram_cmd, "wireguard"))
+        wireguard_handler = CommandHandler('getwireguard',
+                                           partial(telegram_cmd, "wireguard"))
         dispatcher.add_handler(wireguard_handler)
 
-        novpn_handler = CommandHandler('getnoencryptedvpn', partial(telegram_cmd, "novpn"))
+        novpn_handler = CommandHandler('getnoencryptedvpn',
+                                       partial(telegram_cmd, "novpn"))
         dispatcher.add_handler(novpn_handler)
 
-        logging.info('Telegram handlers created')
+        logging.info("Telegram handlers created")
 
         # Starting
         updater.start_polling()
 
     except Exception as err:
-        logging.debug(f'Exception in get_telegram_requests: {err}')
+        logging.debug("Exception in get_telegram_requests: %s", err)
 
 
 def open_imap_connection():
     """
-    Functions opens a connection to an email service, logs in to specified email account,
-    and returns a connection.
+    Functions opens a connection to an email service,
+    logs in to specified email account, and returns a connection.
     """
     # Connect to the server
     connection = imaplib.IMAP4_SSL(IMAP_SERVER)
@@ -95,7 +118,10 @@ def select_inbox_messages():
     global mail
     try:
         mail = open_imap_connection()
-        # Connect to Inbox. Readyonly option: False=marks msgs as read; True=keep messages as unread.
+        # Connect to Inbox
+        # Readyonly option:
+        #     False=marks msgs as read;
+        #     True=keep messages as unread.
         mail.select("Inbox", readonly=False)
         # Search and return UIDS of all UNSEEN/UNREAD emails in Inbox
         status, data = mail.uid('search', None, "UNSEEN")
@@ -105,13 +131,14 @@ def select_inbox_messages():
             if len(id_msg_list) > 0:
                 # Process the new unread emails. If zero, nothing returns
                 for msg_id in id_msg_list:
-                    logging.info("Processing message", msg_id)
-                    # Fetch the email headers and body (RFC822) for the given email UID
+                    logging.info("Processing message %s", msg_id)
+                    # Fetch the email headers and body (RFC822)
+                    # for the given email UID
                     typ, data = mail.uid('fetch', msg_id, '(RFC822)')
                     msg = data[0]
                     yield msg_id, msg
-    except Exception as e:
-        logging.error(f"Exception occurred in select_inbox_messages function {e}")
+    except Exception as err:
+        logging.error("Exception in select_inbox_messages function %s", err)
         return False
     finally:
         mail.expunge()
@@ -162,10 +189,10 @@ def search_for_vpn_keyword(msg):
         try:
             match = re.search(pattern, msg, re.IGNORECASE)
             if match:
-                logging.info(f"Found the correct match for vpn keyword: {match.group(0)}")
+                logging.info("Found the correct match for vpn keyword: %s", match.group(0))
                 return match.group(0)
-        except Exception as e:
-            logging.error(f"Failed to find the correct email subject in parse_email_subject {e}")
+        except Exception as err:
+            logging.error("Failed to find the correct email subject in parse_email_subject %s", err)
             return False
 
 
@@ -185,8 +212,8 @@ def get_email_body_data(message):
     """
     try:
         return message.get_payload().pop().get_payload()
-    except Exception as e:
-        logging.debug(f"Failed with exception {e}. Email body is not in rich email.")
+    except Exception as err:
+        logging.debug("Failed with exception %s. Email body is not in rich email.", err)
         return message.get_payload()
 
 
@@ -227,7 +254,10 @@ def get_email_requests(redis_client):
                 email_to = re.search(r'[\w\.-]+@[\w\.-]+', msg['to']).group(0)
                 email_subject = msg.get("Subject", "empty")
                 email_body = msg_request
-                logging.info(f"Extracted email request: {email_subject}:{email_body}({msg_request})")
+                logging.info("Extracted email request: %s: %s (%s)",
+                             email_subject,
+                             email_body,
+                             msg_request)
                 # Write pending account to provision in REDIS
                 send_request_to_redis(int(email_uid), email_from, msg_type, msg_request, logging, redis_client)
                 # Notify manager of new request
@@ -281,14 +311,14 @@ if __name__ == '__main__':
     try:
         redis_client = redis_connect_to_db(REDIS_SERVER)
     except Exception as err:
-        logging.error(f'Unable to connect to the Redis {REDIS_SERVER}: {err}')
+        logging.error("Unable to connect to the Redis {REDIS_SERVER}: %s", err)
         sys.exit(-1)
 
     # Creating a Redis subscriber
     try:
         db_subscriber = redis_create_subscriber(redis_client)
     except Exception as err:
-        logging.error(f'Unable to create a Redis subscriber: {err}')
+        logging.error("Unable to create a Redis subscriber: %s", err)
         sys.exit(-1)
 
     # Subscribing to Redis channel
@@ -299,29 +329,32 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     try:
-        logging.info("Connection and channel subscription to redis successful.")
+        logging.info("Connection and subscription to redis successful.")
 
         # Starting Telegram bot to check for new messages
-        telegram_bot = threading.Thread(target=get_telegram_requests,
-                                        args=(redis_client, TELEGRAM_BOT_TOKEN, TELEGRAM_START_MSG, TELEGRAM_WAIT_MSG,),
-                                        daemon=True)
+        telegram_bot = threading.Thread(
+                target=get_telegram_requests,
+                args=(redis_client, TELEGRAM_BOT_TOKEN, TELEGRAM_START_MSG, TELEGRAM_WAIT_MSG,),
+                daemon=True)
         telegram_bot.start()
         logging.info("Telegram bot thread started")
 
         # Checking for email messages
         for item in db_subscriber.listen():
             if item['type'] == 'message':
-                logging.info(f"New message received in channel {item['channel']}: {item['data']}")
+                logging.info("New message received in channel %s: %s",
+                             item['channel'],
+                             item['data'])
                 if item['data'] == 'report_status':
                     if get_email_requests(redis_client):
                         redis_client.publish('services_status', 'MOD_COMM_RECV:online')
-                        logging.info('Status Online')
+                        logging.info("Status Online")
                     else:
                         IMAP_SERVER = config['IMAP']['SERVER']
                         IMAP_USERNAME = config['IMAP']['USERNAME']
                         IMAP_PASSWORD = config['IMAP']['PASSWORD']
                         redis_client.publish('services_status', 'MOD_COMM_RECV:error_checking_requests')
-                        logging.info('Error checking requests')
+                        logging.info("Error checking requests")
 
         redis_client.publish('services_status', 'MOD_COMM_RECV:offline')
         logging.info("Terminating.")
@@ -329,7 +362,7 @@ if __name__ == '__main__':
         redis_client.close()
         sys.exit(0)
     except Exception as err:
-        logging.info(f'Terminating via exception in __main__: {err}')
+        logging.info("Terminating via exception in __main__: %s", err)
         db_subscriber.close()
         redis_client.close()
         sys.exit(-1)
